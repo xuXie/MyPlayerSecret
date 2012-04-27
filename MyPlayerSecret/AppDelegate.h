dc732a3eb24531c6e5aa749b2d103f480b57777a
xx <user@xiexudeMac-mini.local>
2012-04-27 15:17:54 +0800

    Initial Commit

---
+++ MyPlayerSecret/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  MyPlayerSecret
+//
+//  Created by xu xie on 12-4-27.
+//  Copyright (c) 2012年 yek.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ MyPlayerSecret/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MyPlayerSecret
+//
+//  Created by xu xie on 12-4-27.
+//  Copyright (c) 2012年 yek.me. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
